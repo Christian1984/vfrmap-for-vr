620c54daae57ce69039c129c073c46361e7ed379
Christian Hoffmann <chris.hoffmann84@gmail.com>
2021-04-04 17:28:07 +0200

    pro: added second iframe and basic buttons for switching between them

--- msfs-panel/christian1984-ingamepanel-vfrmapforvr/html_ui/InGamePanels/CustomPanel/CustomPanel.js
+++ msfs-panel/christian1984-ingamepanel-vfrmapforvr/html_ui/InGamePanels/CustomPanel/CustomPanel.js
@@ -80,7 +80,11 @@ class IngamePanelCustomPanel extends TemplateElement {
         this.ingameUi = this.querySelector('ingame-ui');
 
         this.warningMessage = document.getElementById("WarningMessage");
-        this.iframeElement = document.getElementById("CustomPanelIframe");
+        this.iframe_map = document.getElementById("iframe_map");
+        this.iframe_charts = document.getElementById("iframe_charts");
+
+        this.switch_map = document.getElementById("switch_map");
+        this.switch_charts = document.getElementById("switch_charts");
 
         this.m_MainDisplay = document.querySelector("#MainDisplay");
         this.m_MainDisplay.classList.add("hidden");
@@ -93,16 +97,33 @@ class IngamePanelCustomPanel extends TemplateElement {
                 console.log('panelActive');
                 self.panelActive = true;
                 self.warningMessage.classList.add("show");
-                if (self.iframeElement) {
-                    self.iframeElement.src = 'http://localhost:9000';
+                if (self.iframe_map) {
+                    self.iframe_map.src = 'http://localhost:9000';
+                }
+                if (self.iframe_charts) {
+                    self.iframe_charts.src = 'http://localhost:9000/premium/charts.html';
+                }
+                if(self.switch_map) {
+                    self.switch_map.addEventListener("click", (e) => {
+                        console.log("switch_map clicked!");
+                        this.iframe_map.classList.remove("hidden");
+                        this.iframe_charts.classList.add("hidden");
+                    });
+                }
+                if(self.switch_charts) {
+                    console.log("switch_charts clicked!");
+                    self.switch_charts.addEventListener("click", (e) => {
+                        this.iframe_map.classList.add("hidden");
+                        this.iframe_charts.classList.remove("hidden");
+                    });
                 }
             });
             this.ingameUi.addEventListener("panelInactive", (e) => {
                 console.log('panelInactive');
                 self.panelActive = false;
                 self.warningMessage.classList.remove("show");
-                if (self.iframeElement) {
-                    self.iframeElement.src = '';
+                if (self.iframe_map) {
+                    self.iframe_map.src = '';
                 }
             });
         }
