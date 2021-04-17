e043c3c5e10fba19f7697faaea0d872bc15f21f2
Christian Hoffmann <chris.hoffmann84@gmail.com>
2021-04-17 15:08:40 +0200

    pro: removed dummy charts2.html and added charts.js instead

--- vfrmap-server/vfrmap/html/premium/premium.go
+++ vfrmap-server/vfrmap/html/premium/premium.go
@@ -20,9 +20,9 @@ func (_ FS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
         case "charts.html":
             w.Header().Set("Content-Type", "text/html")
             w.Write(MustAsset("charts.html"))
-        case "charts2.html":
-            w.Header().Set("Content-Type", "text/html")
-            w.Write(MustAsset("charts2.html"))
+        case "charts.js":
+            w.Header().Set("Content-Type", "text/javascript")
+            w.Write(MustAsset("charts.js"))
         case "charts.css":
             w.Header().Set("Content-Type", "text/css")
             w.Write(MustAsset("charts.css"))
