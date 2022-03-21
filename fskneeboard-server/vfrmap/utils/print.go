7bba7512eef1c26b094b63a3ff0fdefd056a9342
Christian Hoffmann <chris.hoffmann84@gmail.com>
2022-03-21 16:01:44 +0100

    implemented output via callback

--- fskneeboard-server/vfrmap/utils/print.go
+++ fskneeboard-server/vfrmap/utils/print.go
@@ -4,23 +4,35 @@ import (
 	"fmt"
 )
 
+var guiPrintCallback func(string)
+
+func SetGuiPrintCallback(callback func(string)) {
+	guiPrintCallback = callback
+}
+
+func printToGui(message string) {
+	if guiPrintCallback != nil {
+		go guiPrintCallback(message)
+	}
+}
+
 func Print(a ...interface{}) {
 	fmt.Print(a...)
 
-	//str := fmt.Sprint(a...)
-	//controlpanel.ConsoleLog(str)
+	str := fmt.Sprint(a...)
+	printToGui(str)
 }
 
 func Printf(format string, a ...interface{}) {
 	fmt.Printf(format, a...)
 
-	//str := fmt.Sprintf(format, a...)
-	//controlpanel.ConsoleLog(str)
+	str := fmt.Sprintf(format, a...)
+	printToGui(str)
 }
 
 func Println(a ...interface{}) {
 	fmt.Println(a...)
 
-	//str := fmt.Sprintln(a...)
-	//controlpanel.ConsoleLog(str)
+	str := fmt.Sprintln(a...)
+	printToGui(str)
 }
