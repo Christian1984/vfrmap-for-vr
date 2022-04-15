12c50d56457802a5e3fa9313e381fda81bc7669f
Christian Hoffmann <chris.hoffmann84@gmail.com>
2022-04-15 17:15:12 +0200

    added a repair script

---
+++ fskneeboard-server/repair-fskneeboard.bat
@@ -0,0 +1,24 @@
+@ECHO off
+ECHO This script tries to repair your FSKneeboard installation by cleaning up the database.
+ECHO:
+ECHO IMPORTANT: Please close FSKneeboard and Flight Simulator before you proceed!
+ECHO:
+ECHO Do you wish to proceed? This will reset your FSKneeboard Database! (The charts folder will NOT be affected)
+SET /P FSKNEEBOARD_CLEANUP="(Y)es / (N)o > "
+
+IF "%FSKNEEBOARD_CLEANUP%" == "Y" GOTO repair
+IF "%FSKNEEBOARD_CLEANUP%" == "y" GOTO repair
+
+:abort
+ECHO:
+ECHO Repair script aborted!
+GOTO end
+
+:repair
+del /s /q .\fskneeboard.db >nul 2>&1
+del /s /q .\fskneeboard.db.lock >nul 2>&1
+ECHO:
+ECHO Repair script finished! Please restart both FSKneeboard and Flight Simulator!
+
+:end
+PAUSE
