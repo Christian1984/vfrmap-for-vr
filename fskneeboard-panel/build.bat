84f44562c34bad4d36ff27673765ef023b303001
Chris <chris.hoffmann84@gmail.com>
2022-05-13 21:47:21 +0200

    added npm run to panel build script

--- fskneeboard-panel/build.bat
+++ fskneeboard-panel/build.bat
@@ -1,2 +1,10 @@
+@ECHO off
+
+ECHO building panel webpack...
+npm run build-panel
+
+ECHO compile panel...
 "%MSFS_SDK%\Tools\bin\fspackagetool.exe" "christian1984-ingamepanel-fskneeboard\Build\christian1984-ingamepanel-fskneeboard.xml" -nomirroring
+
+ECHO copy spb file...
 copy /Y "christian1984-ingamepanel-fskneeboard\Build\Packages\christian1984-ingamepanel-fskneeboard\Build\christian1984-ingamepanel-fskneeboard.spb" "christian1984-ingamepanel-fskneeboard\InGamePanels"
