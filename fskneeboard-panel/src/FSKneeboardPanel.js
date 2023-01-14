77ba910484c6b83d6753669eb02d596802ea97b8
Christian Hoffmann <chris.hoffmann84@gmail.com>
2023-01-14 13:53:39 +0100

    added client side SimVar readouts for testing...

--- fskneeboard-panel/src/FSKneeboardPanel.js
+++ fskneeboard-panel/src/FSKneeboardPanel.js
@@ -1,27 +1,27 @@
-import "./FSKneeboardPanel.scss"
+import "./FSKneeboardPanel.scss";
 
 let MyTemplateElement;
 let myCheckAutoload;
 
 try {
     MyTemplateElement = TemplateElement;
-}
-catch (e) {
+} catch (e) {
     if (parent && parent.window && parent.window.test_environment) {
         MyTemplateElement = class extends HTMLElement {
-            constructor(args) { super(args); }
+            constructor(args) {
+                super(args);
+            }
             connectedCallback() {}
             disconnectedCallback() {}
-        }
+        };
     }
 }
 
 try {
     myCheckAutoload = checkAutoload;
-}
-catch (e) {
+} catch (e) {
     if (parent && parent.window && parent.window.test_environment) {
-        myCheckAutoload = function() {}
+        myCheckAutoload = function () {};
     }
 }
 
@@ -37,13 +37,24 @@ class IngamePanelFSKneeboardPanel extends MyTemplateElement {
         this.collapse_altKey = false;
         this.collapse_ctrlKey = false;
         this.collapse_shiftKey = false;
+        this.posInterval = -1;
+
+        this.logDiv = document.querySelector("#log");
+        if (this.logDiv) {
+            this.logDiv.innerHTML = "<p>log initialized</p>";
+        }
+    }
+
+    log(msg) {
+        if (this.logDiv) {
+            this.logDiv.innerHTML = "<p>" + msg + "</p>" + this.logDiv.innerHTML;
+        }
     }
 
     collapse(collapsed) {
         if (collapsed) {
             this.classList.add("collapsed");
-        }
-        else {
+        } else {
             this.classList.remove("collapsed");
         }
 
@@ -57,10 +68,10 @@ class IngamePanelFSKneeboardPanel extends MyTemplateElement {
     set_brightness(brightness) {
         const overlay_brightness = document.querySelector("#overlay_brightness");
         overlay_brightness.style.opacity = (100 - brightness) / 100;
-        
+
         const overlay_red = document.querySelector("#overlay_red");
         overlay_red.style.filter = "brightness(" + brightness + "%)";
-        overlay_red.style.opacity = - 3 / 1000 * brightness + 1;
+        overlay_red.style.opacity = (-3 / 1000) * brightness + 1;
     }
 
     set_red_light(red) {
@@ -69,8 +80,7 @@ class IngamePanelFSKneeboardPanel extends MyTemplateElement {
         if (panel) {
             if (red) {
                 panel.classList.add("red");
-            }
-            else {
+            } else {
                 panel.classList.remove("red");
             }
         }
@@ -89,11 +99,13 @@ class IngamePanelFSKneeboardPanel extends MyTemplateElement {
                     case "KeyboardEvent":
                         if (self.collapse_hotkey == -1) return;
 
-                        if (data.data.type == "keydown"
-                            && data.data.keyCode == self.collapse_hotkey
-                            && data.data.altKey == self.collapse_altKey
-                            && data.data.ctrlKey == self.collapse_ctrlKey
-                            && data.data.shiftKey == self.collapse_shiftKey) {
+                        if (
+                            data.data.type == "keydown" &&
+                            data.data.keyCode == self.collapse_hotkey &&
+                            data.data.altKey == self.collapse_altKey &&
+                            data.data.ctrlKey == self.collapse_ctrlKey &&
+                            data.data.shiftKey == self.collapse_shiftKey
+                        ) {
                             self.toggle_collapse();
                         }
 
@@ -109,13 +121,12 @@ class IngamePanelFSKneeboardPanel extends MyTemplateElement {
                     case "SetBrighness":
                         self.set_brightness(data.data.brightness);
                         break;
-                        
+
                     case "SetRedLight":
                         self.set_red_light(data.data.red);
                         break;
                 }
-            }
-            catch (e) {
+            } catch (e) {
                 /* ignore silently */
             }
         });
@@ -123,10 +134,12 @@ class IngamePanelFSKneeboardPanel extends MyTemplateElement {
         window.addEventListener("keydown", (e) => {
             if (self.collapse_hotkey == -1) return;
 
-            if (e.keyCode == self.collapse_hotkey
-                && e.altKey == self.collapse_altKey
-                && e.ctrlKey == self.collapse_ctrlKey
-                && e.shiftKey == self.collapse_shiftKey) {
+            if (
+                e.keyCode == self.collapse_hotkey &&
+                e.altKey == self.collapse_altKey &&
+                e.ctrlKey == self.collapse_ctrlKey &&
+                e.shiftKey == self.collapse_shiftKey
+            ) {
                 self.toggle_collapse();
             }
         });
@@ -142,45 +155,61 @@ class IngamePanelFSKneeboardPanel extends MyTemplateElement {
                     self.warning_message.classList.add("show");
 
                     if (self.content_iframe) {
-                        self.content_iframe.src = 'http://localhost:9000/index.html';
+                        self.content_iframe.src = "http://localhost:9000/index.html";
                     }
 
+                    if (this.posInterval == -1) {
+                        this.posInterval = setInterval(() => {
+                            try {
+                                const lat = SimVar.GetSimVarValue("PLANE LATITUDE", "degrees");
+                                const lng = SimVar.GetSimVarValue("PLANE LONGITUDE", "degrees");
+                                const alt = SimVar.GetSimVarValue("A:INDICATED ALTITUDE", "meters");
+                                this.log("Lat: " + lat + ", Lng: " + lng + ", alt: " + alt + "m");
+                            } catch (e) {
+                                this.log(e);
+                            }
+                        }, 5000);
+                    }
                     self.collapse(false);
                 });
-    
+
                 this.ingameUi.addEventListener("panelInactive", (e) => {
                     self.panelActive = false;
                     self.warning_message.classList.remove("show");
-    
+                    clearInterval(this.posInterval);
+                    this.posInterval = -1;
+
                     if (self.content_iframe) {
-                        self.content_iframe.src = '';
+                        self.content_iframe.src = "";
                     }
                 });
             }
-        } , 0);
+        }, 0);
     }
 
     disconnectedCallback() {
         super.disconnectedCallback();
     }
 
-    updateImage() {
-
-    }
+    updateImage() {}
 }
 
 window.customElements.define("ingamepanel-custom", IngamePanelFSKneeboardPanel);
 myCheckAutoload();
 
 if (parent && parent.window && parent.window.test_environment) {
-    parent.document.addEventListener('testReady', function () {
-        const uis = document.querySelectorAll("ingame-ui");
+    parent.document.addEventListener(
+        "testReady",
+        function () {
+            const uis = document.querySelectorAll("ingame-ui");
 
-        for (let ui of uis) {
-            const event = new Event('panelActive');
-            setTimeout(() => {
-                ui.dispatchEvent(event);
-            }, 250);
-        }
-    }, false);
+            for (let ui of uis) {
+                const event = new Event("panelActive");
+                setTimeout(() => {
+                    ui.dispatchEvent(event);
+                }, 250);
+            }
+        },
+        false
+    );
 }
